c05e89c5a344cbe2417a7e9d6f7db3fefde3166a
tizihoxha <tizihoxha@gmail.com>
2023-01-04 15:18:15 +0100

    task4

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-apt-get update && apt-get install -y hugo make
-sudo snap install hugo
-sudo apt update
-sudo apt install make
+apt-get update && apt-get install -y make build-essential wget && wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb && dpkg -i hugo_extended_0.109.0_linux-amd64.deb
+make build
+
+
+
+
+
+
+
+
+
+
+
